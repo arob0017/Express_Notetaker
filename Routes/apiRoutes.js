96447d76343f25516e269a798cf2a572cb804592
Amelia Roberts <arob0017@student.monash.edu>
2020-10-21 12:54:49 +1100

    added api routes and fixed pathways

---
+++ Routes/apiRoutes.js
@@ -0,0 +1,44 @@
+
+const fs = require("fs");
+let notes = require("../db/db.json")
+
+function apiRoutes(app) {
+    // Displays note data
+    app.get("api/notes", (req, res) => {
+        res.json(notes);
+    });
+
+    // adds route
+    app.post("/api/notes", (req, res) => {
+        let newNote = req.body;
+        (notes.length === 0)
+        newNote.push(newNote);
+        updateDb();
+        return console.log("Added new note " + newNote.title)
+
+    });
+
+    // Retrieves a note with specific id
+    app.get("/api/notes/:id", function (req, res) {
+        // display json for the notes array indices of the provided id
+        res.json(notes[req.params.id]);
+    });
+
+    // deletes route
+    app.delete("/api/notes/:id", (req, res) => {
+        notes.splice(req.params.id, 1);
+        updateDb();
+        console.log("Deleted note" + req.params.id);
+
+        // res.json({ success: true });
+    });
+
+    // updates the json file anytime something is added or deleted
+    function updateDb() {
+        fs.writeFile("db/db.json", JSON.stringify(notes, '\t'), err => {
+            if (err) throw err;
+            return true;
+        });
+    }
+};
+module.exports = apiRoutes;
--- Routes/htmlRoutes.js
+++ Routes/htmlRoutes.js
@@ -1,12 +1,12 @@
 var path = require("path");
 function htmlRoutes(app) {
     app.get("/notes", function (req, res) {
-        res.sendFile(path.join(__dirname, "/public/notes.html"));
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
 
     // sets index as default if routes don't match
     app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "/public/index.html"));
+        res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 };
 module.exports = htmlRoutes;
